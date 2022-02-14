1d80dd8de5ec076562d3868534526f9af6cc0f28
Maksim Kalugin <wolfqest24@gmail.com>
2022-02-14 17:05:45 +0500

    add comments

--- Client3.py
+++ Client3.py
@@ -5,16 +5,20 @@ import threading
 disconnect = False
 join = False
 
+
+# Получаем адрес сервера, в данном случае он совпадает с нашей машиной
 server_address = socket.gethostbyname(socket.gethostname())
 server_port = 9090
 client_port = 0
+server = (server_address, server_port)
 
-server = (server_address, server_port)# 192.168.56.1
-
+# Создаём соединение на IPv4 и UDP и привязываем его к нашему адресу и порту
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((server_address, client_port))
 s.setblocking(False)
 
+
+# Логика получения сообщений
 def receving(self, sock):
     while not disconnect:
         try:
@@ -31,6 +35,7 @@ nickname = input("Nickname: ")
 receiv_thread = threading.Thread(target=receving, args=('', s))
 receiv_thread.start()
 
+# Отправка сообщений и статусов пользователя
 while not disconnect:
     if not join:
         s.sendto(("[" + time.strftime("%H:%M:%S", time.localtime()) + "]" + "[" + nickname + "]" + " >>> join chat ").encode("utf-8"), server)
--- Server.py
+++ Server.py
@@ -1,18 +1,25 @@
 import socket
 import time
 
+
+# Получаем адрес нашей машины и определяем порт
+# Порт выбираем в диапазоне от 1025 до 65000
 ip = socket.gethostbyname(socket.gethostname())
 port = 9090
 
 print(ip)
 clients = []
 
+
+# Создаём соединение сервeра на IPv4 и UDP и привязываем его к нашему адресу и порту
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((ip, port))
 
 disconnect = False
 print("[ Server Started ]")
 
+
+# Логика работы сервера, пока есть подключение
 while not disconnect:
     try:
         data, addr = server.recvfrom(1024)
@@ -20,9 +27,9 @@ while not disconnect:
         if addr not in clients:
             clients.append(addr)
 
-        itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+        time_mess = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
 
-        print("[" + addr[0] + "]=[" + str(addr[1]) + "]=[" + itsatime + "]/", end="")
+        print("[" + addr[0] + "]=[" + str(addr[1]) + "]=[" + time_mess + "]/", end="")
         print(data.decode("utf-8"))
 
         for client in clients:
